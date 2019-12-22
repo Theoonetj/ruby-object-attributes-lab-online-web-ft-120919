792c8f8811610bf53babf7b592ed694a3be8864e
confident-token-1019 <theoonetj@gmail.com>
2019-12-22 03:02:24 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def personName=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end 
+
+  def job 
+    @job
+  end 
+  
+end
